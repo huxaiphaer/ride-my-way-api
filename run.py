81a624c187e7e33cf64522caaf2ed66a49a7cf98
huxaiphaer <huxaiphaeridris@gmail.com>
2018-06-20 20:02:13 +0300

    update the run.py

--- run.py
+++ run.py
@@ -2,5 +2,9 @@ from flask import Flask
 from flask_restful import Resource, Api
 from app import app
 
+@app.route('/', methods=['GET'])
+def index():
+    return ('welcome to ride my way API. Test the endpoints in postman')
+
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
